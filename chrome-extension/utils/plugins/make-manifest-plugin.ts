16486d2f9f6170001640a4aa59d31d40ac1ec7b2
PatrykKuniczak <p.kuniczak@gmail.com>
2024-09-06 17:35:57 +0200

    feat: create ts support for all modules and refactor manifest file and i18n module

--- chrome-extension/utils/plugins/make-manifest-plugin.ts
+++ chrome-extension/utils/plugins/make-manifest-plugin.ts
@@ -2,37 +2,40 @@ import fs from 'node:fs';
 import { resolve } from 'node:path';
 import { pathToFileURL } from 'node:url';
 import process from 'node:process';
+import type { Manifest } from '@extension/dev-utils';
 import { colorLog, ManifestParser } from '@extension/dev-utils';
 import type { PluginOption } from 'vite';
 
-const rootDir = resolve(__dirname, '..', '..');
-const manifestFile = resolve(rootDir, 'manifest.js');
+const manifestFile = resolve(import.meta.dirname, '..', '..', 'manifest.js');
 
-const getManifestWithCacheBurst = (): Promise<{ default: chrome.runtime.ManifestV3 }> => {
+const getManifestWithCacheBurst = async () => {
   const withCacheBurst = (path: string) => `${path}?${Date.now().toString()}`;
+
+  let rawManifest: { default: Manifest };
   /**
    * In Windows, import() doesn't work without file:// protocol.
    * So, we need to convert path to file:// protocol. (url.pathToFileURL)
    */
   if (process.platform === 'win32') {
-    return import(withCacheBurst(pathToFileURL(manifestFile).href));
+    rawManifest = await import(withCacheBurst(pathToFileURL(manifestFile).href));
+  } else {
+    rawManifest = await import(withCacheBurst(manifestFile));
   }
-
-  return import(withCacheBurst(manifestFile));
+  return rawManifest.default;
 };
 
-export default function makeManifestPlugin(config: { outDir: string }): PluginOption {
-  function makeManifest(manifest: chrome.runtime.ManifestV3, to: string) {
+export default (config: { outDir: string }): PluginOption => {
+  const makeManifest = (manifest: Manifest, to: string) => {
     if (!fs.existsSync(to)) {
       fs.mkdirSync(to);
     }
-    const manifestPath = resolve(to, 'manifest.json');
 
+    const manifestPath = resolve(to, 'manifest.json');
     const isFirefox = process.env.__FIREFOX__ === 'true';
-    fs.writeFileSync(manifestPath, ManifestParser.convertManifestToString(manifest, isFirefox ? 'firefox' : 'chrome'));
+    fs.writeFileSync(manifestPath, ManifestParser.convertManifestToString(manifest, isFirefox));
 
     colorLog(`Manifest file copy complete: ${manifestPath}`, 'success');
-  }
+  };
 
   return {
     name: 'make-manifest',
@@ -42,7 +45,7 @@ export default function makeManifestPlugin(config: { outDir: string }): PluginOp
     async writeBundle() {
       const outDir = config.outDir;
       const manifest = await getManifestWithCacheBurst();
-      makeManifest(manifest.default, outDir);
+      makeManifest(manifest, outDir);
     },
   };
-}
+};
